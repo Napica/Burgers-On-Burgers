0b144d2a7b476addceaff399e8e234c6a5324e38
Archangel <nathapcal@gmail.com>
2020-10-07 21:39:27 -0400

    updated server.js to reach to the port in express

---
+++ server.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES 
+var exphbs = require("express-handlebars");
+var express = require("express");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// MIDDLEWARE
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+// LISTENING TO PORTS
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
