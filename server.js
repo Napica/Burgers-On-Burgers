77730bd90350c14c1885785cb909f1234feba7b0
Archangel <nathapcal@gmail.com>
2020-10-07 23:08:00 -0400

    updated the path for the files to clean up the connection

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 // DEPENDENCIES 
-var exphbs = require("express-handlebars");
-var express = require("express");
-var mysql = require("mysql");
-var connection = require("./config/connection")
+const exphbs = require("express-handlebars");
+const express = require("express");
+const mysql = require("mysql");
 
-var app = express();
+
+const app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-// MIDDLEWARE
+// EXPRESS MIDDLEWARE
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// HANDLEBAR's MIDDLEWARE
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+
+// Imports routes and able to access the server 
+const routes = require ("./controllers/burgers_controller.js")
+app.use(routes);
 
 // LISTENING TO PORTS
 // Start our server so that it can begin listening to client requests.
