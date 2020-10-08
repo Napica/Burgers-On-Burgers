755e5567f7045b6c256629090dff779ff3a77fc5
Archangel <nathapcal@gmail.com>
2020-10-07 21:41:41 -0400

    added connection to mysql and passed test to connect

--- server.js
+++ server.js
@@ -20,6 +20,22 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "shepard1",
+    database: "burgers_db"
+  });
+  
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
 
 
 // LISTENING TO PORTS
