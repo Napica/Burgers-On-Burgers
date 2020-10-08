b39c422231b8c1a48ad63379c6eb3ea644c91378
Archangel <nathapcal@gmail.com>
2020-10-08 14:31:05 -0400

    minor filename updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-var burgers = require ("../models/burgers.js")
+var burger = require ("../models/burger.js")
 
 router.get ("/", (req,res)=> {
     res.render("index");
