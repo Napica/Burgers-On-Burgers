09d139dfdf60a2e26bc08209ff914b5bc37f134f
Archangel <nathapcal@gmail.com>
2020-10-09 16:06:12 -0400

    delete burger button is functionable and interacts with the database

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,12 +1,11 @@
-
 // Change function to move burger to devoured section.
 $(function () {
   $(".change-devoured").on("click", function (event) {
     // console.log("this was clicked")
-    
+
     var id = $(this).data("id");
     var newDevoured = $(this).data("newdevour");
-    
+
     var newDevouredState = {
       devoured: newDevoured,
     };
@@ -21,21 +20,19 @@ $(function () {
       location.reload();
     });
   });
-});
 
+  // Function to remove the burger
+  $(".delete-burger").on("click", function (event) {
+    // console.log("this clicked")
+    var id = $(this).data("id");
 
-// Function to remove the burger
-$(".delete-burger").on("click", function(event) {
-  var id = $(this).data("id");
-
-  // Send the DELETE request.
-  $.ajax("/api/burgers/" + id, {
-    type: "DELETE"
-  }).then(
-    function() {
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(function () {
       console.log("deleted burger", id);
       // Reload the page to get the updated list
       location.reload();
-    }
-  );
+    });
+  });
 });
