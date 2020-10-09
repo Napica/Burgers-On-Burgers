1bca8a074ed9c5efc2dd2891e98518cea95b4890
Archangel <nathapcal@gmail.com>
2020-10-09 16:23:52 -0400

    able to add a burger by name

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -35,4 +35,29 @@ $(function () {
       location.reload();
     });
   });
+
+  // Function to create a new burger
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+
 });
